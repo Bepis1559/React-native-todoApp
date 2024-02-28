ff4ecdc6e8acbbb248b23636497ecbc0faf6ba24
Bepis1559 <axl2001@abv.bg>
2024-02-28 11:27:04 +0200

    added motion

--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -7,6 +7,7 @@ import {
   CheckboxLabel,
 } from "@gluestack-ui/themed";
 import { useState, type ReactElement } from "react";
+import { Motion } from "@legendapp/motion";
 
 type props = {
   value: string;
@@ -17,33 +18,37 @@ export function ToDo({ value }: props): ReactElement {
     setDone((prev) => !prev);
   }
   return (
-    <Box
-      sx={{
-        marginBottom: 15,
-        paddingVertical: 15,
-        paddingHorizontal: 30,
-        borderRadius: 7,
-        backgroundColor: "rgb(23, 23, 23)",
-      }}>
-      <Checkbox
-        size="md"
-        aria-label={value}
-        value={value}
-        accessibilityLabel="Checkbox"
-        onChange={handleOnChange}
-        nativeID="checkbox-2">
-        <CheckboxIndicator sx={{ borderRadius: "$full" }} mr="$2">
-          <CheckboxIcon as={CheckIcon} />
-        </CheckboxIndicator>
-        <CheckboxLabel
-          sx={{
-            textDecorationStyle: "solid",
-            textDecorationLine: done ? "line-through" : "none",
-            textDecorationColor: "$white",
-          }}>
-          {value}
-        </CheckboxLabel>
-      </Checkbox>
-    </Box>
+    <Motion.View
+      animate={{ y: done ? 300 : 0 }}
+      transition={{ type: "timing", duration: 1000, easing: "linear" }}>
+      <Box
+        sx={{
+          marginBottom: 15,
+          paddingVertical: 15,
+          paddingHorizontal: 30,
+          borderRadius: 7,
+          backgroundColor: "rgb(23, 23, 23)",
+        }}>
+        <Checkbox
+          size="md"
+          aria-label={value}
+          value={value}
+          accessibilityLabel="Checkbox"
+          onChange={handleOnChange}
+          nativeID="checkbox-2">
+          <CheckboxIndicator sx={{ borderRadius: "$full" }} mr="$2">
+            <CheckboxIcon as={CheckIcon} />
+          </CheckboxIndicator>
+          <CheckboxLabel
+            sx={{
+              textDecorationStyle: "solid",
+              textDecorationLine: done ? "line-through" : "none",
+              textDecorationColor: "$white",
+            }}>
+            {value}
+          </CheckboxLabel>
+        </Checkbox>
+      </Box>
+    </Motion.View>
   );
 }
