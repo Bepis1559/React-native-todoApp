3649b33cabc667c1a677d0e1406d9999c8ae4c30
Bepis1559 <axl2001@abv.bg>
2024-02-29 18:20:48 +0200

    fixed stuff for android

--- components/CheckBoxTextContent.tsx
+++ components/CheckBoxTextContent.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from "@gluestack-ui/themed";
 import { AnimatedTextCross } from "./AnimatedTextCross";
 import { type ReactElement } from "react";
+import { isPast, parse } from "date-fns";
 
 type CheckBoxTextContentProps = {
   isTodoCompleted: boolean;
@@ -15,23 +16,29 @@ export function CheckBoxTextContent({
   value,
   dueDate,
 }: CheckBoxTextContentProps): ReactElement {
+  const parsedDate = dueDate
+    ? parse(dueDate, "MMM d, yyyy, h:mm aa", new Date())
+    : null;
+  const isOverdue = parsedDate ? isPast(parsedDate) : false;
   return (
-    <Box>
-      <Text
-        sx={{
-          width: "max-content",
-          position: "relative",
-        }}>
-        <AnimatedTextCross
-          isTodoCompleted={isTodoCompleted}
-          animationDuration={animationDuration}
-        />
-        {value}
-      </Text>
-      <br />
-      <Text sx={{ fontSize: "$xs", color: "$coolGray300" }}>
-        {dueDate?.toString()}
-      </Text>
+    <Box marginLeft={5}>
+      <Box alignSelf="flex-start">
+        <Box position="absolute" top={0} left={0} right={0} bottom={0}>
+          <AnimatedTextCross
+            isTodoCompleted={isTodoCompleted}
+            animationDuration={animationDuration}
+          />
+        </Box>
+        <Text>{value}</Text>
+      </Box>
+      <Box>
+        <Text
+          fontSize={"$xs"}
+          display={dueDate ? "flex" : "none"}
+          color={isOverdue ? "#cf2b39" : "$coolGray400"}>
+          {dueDate}
+        </Text>
+      </Box>
     </Box>
   );
 }
--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -10,28 +10,32 @@ type props = {
 };
 export function ToDo({ value, dueDate, id }: props): ReactElement {
   const [completed, setCompleted] = useState(false);
-  // ADD OVERDUE NEXT
+  const [scale, setScale] = useState(1);
+
+  const labelCrossingAnimationDuration = useRef(300);
+  const todoAnimationTime = useRef(700);
+  const verticalDistance = useRef(13.5);
+  const handleOnChange = () => setCompleted((prev) => !prev);
+  const handleTouchStart = () => setScale(1.2);
+  const handleTouchEnd = () => setScale(1);
+
   // add modal on click
   // add submit to db button , later on
 
-  const labelCrossingAnimationDuration = useRef(300);
-  const todoAnimationTime = useRef(1200);
-  const verticalDistance = useRef(13.5);
-  function handleOnChange() {
-    setCompleted((prev) => !prev);
-  }
   return (
     <Motion.View
       style={{
         flexDirection: "row",
-
         alignItems: "center",
         marginBottom: verticalDistance.current,
         paddingVertical: verticalDistance.current,
         paddingHorizontal: 10,
         borderRadius: 10,
         backgroundColor: "rgb(34, 34, 34)",
+        transform: [{ scale: scale }],
       }}
+      // onTouchEnd={handleTouchEnd}
+      // onTouchStart={handleTouchStart}
       animate={{ y: completed ? 300 : 0 }}
       transition={{
         type: "timing",
