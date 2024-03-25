4f8ec6edd9d4cbd9826d74ee3d4d168e2f0a6076
Bepis1559 <axl2001@abv.bg>
2024-03-25 20:21:38 +0200

    removed expanded todo component

--- app/todos/[id].tsx
+++ app/todos/[id].tsx
@@ -1,16 +1,19 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useRef } from "react";
 import { AppContainer } from "../../wrappers/AppContainer";
-import { ExpandedTodo } from "../../components/expandedTodo/ExpandedTodo";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useAtomValue } from "jotai";
 import { isTextContentInteractedWithAtom } from "../../context/expandedTodoContext";
 import { Expanded_headerRight } from "../../components/expandedTodo/Expanded_headerRight";
+import { Expanded_content } from "../../components/expandedTodo/Expanded_content";
+import { Expanded_date } from "../../components/expandedTodo/Expanded_date";
+import { Expanded_remarks } from "../../components/expandedTodo/Expanded_remarks";
 
 export default function Page(): ReactElement {
   const navigation = useNavigation();
   const { id, isCompleted, value, dueDate, remarks } =
     useLocalSearchParams() as unknown as expandedTodoProps;
   const isInteracting = useAtomValue(isTextContentInteractedWithAtom);
+  const textColor = useRef("#528deb");
 
   useEffect(() => {
     navigation.setOptions({
@@ -19,13 +22,14 @@ export default function Page(): ReactElement {
   }, [isInteracting, navigation]);
   return (
     <AppContainer>
-      <ExpandedTodo
-        remarks={remarks}
+      <Expanded_content
         id={id}
         isCompleted={isCompleted}
         value={value}
-        dueDate={dueDate}
+        textColor={textColor.current}
       />
+      <Expanded_date textColor={textColor.current} dueDate={dueDate} />
+      <Expanded_remarks remarks={remarks} textColor={textColor.current} />
     </AppContainer>
   );
 }
--- components/expandedTodo/Expanded_TextContent.tsx
+++ components/expandedTodo/Expanded_TextContent.tsx
@@ -1,4 +1,4 @@
-import { type ReactElement, useRef, useState, memo } from "react";
+import { type ReactElement, useState, memo } from "react";
 import { TextInput } from "react-native";
 import { getExpandedTodo_contentInputStyle } from "../../styles/ExpandedTodoStyle";
 import { useSetAtom } from "jotai";
@@ -6,19 +6,15 @@ import { isTextContentInteractedWithAtom } from "../../context/expandedTodoConte
 
 function Component(props: Expandend_TextContentProps): ReactElement {
   const { tempCompleted, textColor, initialInputValue } = props;
-  const inputRef = useRef<TextInput>(null);
   const [inputState, setInputState] = useState(initialInputValue);
   const setIsInteracting = useSetAtom(isTextContentInteractedWithAtom);
-  const handleInteraction = () => setIsInteracting(true);
-
   return (
     <TextInput
-      ref={inputRef}
       style={getExpandedTodo_contentInputStyle(tempCompleted)}
       multiline
       selectionColor={textColor}
+      onFocus={() => setIsInteracting(true)}
       value={inputState}
-      onFocus={handleInteraction}
       onChangeText={(newText) => setInputState(newText)}
     />
   );
