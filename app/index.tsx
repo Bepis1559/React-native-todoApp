bf33d691abfc7ff05c04320c99ad8b6ea73a6455
Bepis1559 <axl2001@abv.bg>
2024-03-26 19:57:43 +0200

    configered eas for building

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+// building command as APK - eas build -p android --profile preview
 import { Box } from "@gluestack-ui/themed";
 import React from "react";
 import { AllToDos } from "../components/allTodos/AllToDos";
